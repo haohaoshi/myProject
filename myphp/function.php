e2fb17cea6b541ce9bac7fbc0a793c667fc905cf
lujz <lujz@cnfol.net>
2018-11-05 10:25:27 +0800

    修改

--- myphp/function.php
+++ myphp/function.php
@@ -47,6 +47,18 @@ function uncertainParam() {
     //var_export($args);
 }
 
+//日志输出
+function log_ljz($data,$file_name='log_ljz')
+{
+    if(is_array($data))$data = var_export($data,true);
+    $file=dirname(__FILE__)."/{$file_name}.log";
+    $res=debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+    $log = "";
+    $log .= "执行日期：".strftime("%Y-%m-%d %H:%M:%S",time())."\n ";
+    $log .= "文件所在位置：".$res[0]['file'].",位于第".$res[0]['line']."行\n";
+    $log .= $data."\n\n";
+    file_put_contents($file,$log,FILE_APPEND|LOCK_EX);
+}
 
 
 
